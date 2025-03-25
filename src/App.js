18cacdbb14c152cba68532ee6e80995c178292ac
Bertrand Shao <bertrandshao@gmail.com>
2025-03-24 17:36:31 -0700

    implement richer than ts log level

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import './App.css';
-import { createLogger } from './Logger/createLogger';
+import "./App.css";
+import { createLogger } from "./Logger/createLogger";
 
 function App() {
-  const logger = createLogger()
+  const logger = createLogger();
 
-  logger.debug("check it out", "data")
-
-  return (
-    <div className="App">
-      A custom functional logger implementation.
-    </div>
-  );
+  return <div className="App">A custom functional logger implementation.</div>;
 }
 
 export default App;
--- src/Logger/LogLevel.ts
+++ src/Logger/LogLevel.ts
@@ -1,4 +1,14 @@
 /*
+
+enum LogLevel {
+  INFO,
+  WARN,
+  ERROR
+}
+
+Unfortunately the above option is not robust enough. Because Typescript cares about the structure of data and because each enum don't have an associated value, Typescript sees each as comparable to an empty object {}.
+For our logger to work, we must be able to compare the weight of a level for filtering purposes. We also need access to both the string and numerical value from the LogLevel directly.
+
 Goals:
 
 - Satisfies `instanceof` comparison: `LogLevel.DEBUG instanceof LogLevel === true`
@@ -6,3 +16,49 @@ Goals:
 - Can be order-compared using `>`, `>=`, `<`, `<=` for log filtering
 - Can output the correct string representation using `String(LogLevel.DEBUG)
 */
+
+const instSymbol: unique symbol = Symbol("LogLevelInst");
+
+type LogLevelInstanceSymbol = typeof instSymbol;
+
+type LogLevel = { [Key in LogLevelInstanceSymbol]: true };
+
+let isClosed = false;
+
+const LogLevel = (name, value) => {
+  if (isClosed) {
+    throw new Error("LogLevel has been frozen");
+  }
+  const inst = Object.create(LogLevel.prototype);
+  Object.assign(inst, {
+    [Symbol.toPrimitive](hint: string) {
+      return hint === "string" ? name : value;
+    },
+    valueOf() {
+      return value;
+    },
+    toString() {
+      return name;
+    },
+    toJson() {
+      return name;
+    },
+    [instSymbol]: true,
+  });
+  return Object.freeze(inst);
+};
+
+LogLevel.prototype = {
+  constructor: LogLevel,
+};
+
+LogLevel.DEBUG = LogLevel("DEBUG", 10);
+LogLevel.INFO = LogLevel("INFO", 20);
+LogLevel.WARN = LogLevel("WARN", 30);
+LogLevel.ERROR = LogLevel("ERROR", 40);
+LogLevel.FATAL = LogLevel("FATAL", 50);
+LogLevel.DISABLED = LogLevel("DISABLED", 100);
+
+isClosed = true;
+
+export { LogLevel };
